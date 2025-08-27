8c375c7897c3dac86a298fc3c07f49ad5befe1d1
kimgho <kgh67811@knu.ac.kr>
2025-08-28 03:12:50 +0900

    fix: layout을 전역으로 적용되도록 수정

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 export const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="min-h-screen">
-      <div className="max-w-xl mx-auto px-4 md:max-w-md lg:max-w-lg bg-gradient-primary">
+      <div className="max-w-full mx-auto px-4 md:max-w-md lg:max-w-lg bg-gradient-primary">
         {children}
       </div>
     </div>
