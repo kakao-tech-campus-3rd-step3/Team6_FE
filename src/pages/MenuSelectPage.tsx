fc88f451e5d8729ca096c89ff99ae90555d7d5f9
cktjgus <cktjgus04@naver.com>
2025-09-04 02:38:29 +0900

    feat(menu): 역할별 조건부 렌더링 임시 구현

--- src/pages/MenuSelectPage.tsx
+++ src/pages/MenuSelectPage.tsx
@@ -1,21 +1,22 @@
 import { IntroSection } from "@/components/menuselect/IntroSection";
 import { RoleBadge } from "@/components/menuselect/RoleBadge";
-//import { MenuList } from "@/components/menuselect";
+import { MenuList } from "@/components/menuselect";
 import { MenuWaiting } from "@/components/menuselect/MenuWaiting";
 import { AppScreen } from "@stackflow/plugin-basic-ui";
 import type { ActivityComponentType } from "@stackflow/react";
 
 const MenuSelectPage: ActivityComponentType = () => {
   // 나중에 로그인 사용자 정보나 방 참여 API로 role 가져오기
-  const role = "구성원"; // or "방장"
+  const role = "방장"; // or "구성원"
 
   return (
     <AppScreen appBar={{ title: "아이스브레이킹" }}>
       <main className="bg-gradient-primary space-y-4 p-4 pb-8">
         <IntroSection />
         <RoleBadge role={role} />
-        {/*<MenuList />*/}
-        <MenuWaiting />
+
+        {role === "방장" ? <MenuList /> : <MenuWaiting />} 
+        {/* 현재는 고정된 role 값 기준으로 분기 처리. 추후 role 값을 받아와 수정*/}
       </main>
     </AppScreen>
   );
