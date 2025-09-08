ba061f56db0d6e73dcd5e6096e75923dae9d65dd
kimgho <kgh67811@knu.ac.kr>
2025-09-09 01:37:12 +0900

    feat(constants): mbti 상수 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,4 @@ export { type InterestType, INTERESTS } from "./interests";
 export { DEFAULT_QUESTIONS } from "./default-questions";
 export { COLORS, COLOR_MAP } from "./roulette-color";
 export * from "./time-number";
+export { mbtiPairs } from "./mbti";
