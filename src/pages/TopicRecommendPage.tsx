9fe2a405f40d8e245d473fb049291133d8dfde9d
cktjgus <cktjgus04@naver.com>
2025-09-08 21:17:33 +0900

    feat(stackflow): TopicRecommendPage 추가

---
+++ src/pages/TopicRecommendPage.tsx
@@ -0,0 +1,15 @@
+import { AppScreen } from "@stackflow/plugin-basic-ui";
+import type { ActivityComponentType } from "@stackflow/react";
+
+
+const TopicRecommendPage: ActivityComponentType = () => {
+
+  return (
+    <AppScreen appBar={{ title: "주제 추천" }}>
+      <main className="bg-gradient-primary space-y-4 p-4 pb-8">
+      </main>
+    </AppScreen>
+  );
+};
+
+export default TopicRecommendPage;
