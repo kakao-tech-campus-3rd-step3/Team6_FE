d4cdf4e6304b15f059a3b2462eec9e68b40f53f0
kimgho <kgh67811@knu.ac.kr>
2025-09-09 01:05:15 +0900

    feat(constants): 상수 배럴패턴 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
-export { INTERESTS } from "./interests";
+export { type InterestType, INTERESTS } from "./interests";
 export { DEFAULT_QUESTIONS } from "./default-questions";
 export { COLORS, COLOR_MAP } from "./roulette-color";
+export * from "./time-number";
