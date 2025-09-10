a499dd7f53d640f0d1fdaa53652f93c5a5057988
wnhaoo <153691178+wnhaoo@users.noreply.github.com>
2025-09-10 15:54:37 +0900

    chore(manitto): 데이터 수정

--- src/pages/ManittoPage.tsx
+++ src/pages/ManittoPage.tsx
@@ -9,8 +9,9 @@ const ManittoPage: ActivityComponentType<"ManittoPage"> = () => {
     name: "김민수",
     age: 25,
     mbti: "ENFP",
-    interests: ["영화", "음악", "여행", "운동"],
-    intro: "새로운 사람들과 만나는 것을 좋아해요! 함께 즐거운 시간 보내요 😊",
+    interests: ["스포츠", "음악", "영화", "독서", "여행", "요리", "게임", "동물"],
+    intro:
+      "새로운 사람들과 만나는 것을 좋아해요! 함께 즐거운 시간 보내요 새로운 사람들과 만나는 것을 좋아해요! 함께 즐거운 시간 보내요 새로운 사람들과 만나는 것을 좋아해요! 함께 즐거운",
   };
 
   return (
