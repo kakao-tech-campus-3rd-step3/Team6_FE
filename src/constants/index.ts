63ebaa8060744fc77c27beaed4697c4a1b0d8140
kimgho <kgh67811@knu.ac.kr>
2025-09-12 01:55:22 +0900

    feat(constants): STOMP적용 할 경로 상수 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,3 +3,4 @@ export { DEFAULT_QUESTIONS } from "./default-questions";
 export { COLORS, COLOR_MAP } from "./roulette-color";
 export * from "./time-number";
 export { mbtiPairs } from "./mbti";
+export { STOMP_REQUIRED_PATHS } from "./stomp-path";
