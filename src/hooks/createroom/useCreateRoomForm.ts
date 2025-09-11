f1d7ba00d2bb29b62ee46bfde770e97eab1e64a1
kimgho <kgh67811@knu.ac.kr>
2025-09-12 02:26:04 +0900

    feat(hooks): 방 생성을 위한 커스텀 훅 추가

---
+++ src/hooks/createroom/useCreateRoomForm.ts
@@ -0,0 +1,68 @@
+import { useSingleSelection } from "@/hooks";
+import { useMemo, useState } from "react";
+
+export interface CreateRoomFormData {
+  roomName: string;
+  capacity: number;
+  purpose: "business" | "personal" | "drinking" | "";
+}
+
+const initialFormData: CreateRoomFormData = {
+  roomName: "",
+  capacity: 2,
+  purpose: "",
+};
+
+const MIN_PARTICIPANT = 2;
+const MAX_PARTICIPANT = 20;
+
+export const useCreateRoomForm = () => {
+  const [formData, setFormData] = useState<CreateRoomFormData>(initialFormData);
+  const {
+    selectItem: selectPurpose,
+    isSelected: isPurposeSelected,
+    selection: selectedPurpose,
+  } = useSingleSelection<CreateRoomFormData["purpose"]>();
+
+  const updateRoomName = (roomName: string) => {
+    setFormData((prev) => ({ ...prev, roomName }));
+  };
+
+  const updateCapacity = (capacity: number) => {
+    if (isNaN(capacity)) {
+      setFormData((prev) => ({ ...prev, capacity: 2 }));
+    } else {
+      setFormData((prev) => ({ ...prev, capacity }));
+    }
+  };
+
+  const updatePurpose = (purpose: CreateRoomFormData["purpose"]) => {
+    selectPurpose(purpose);
+  };
+
+  const finalFormData = useMemo(
+    () => ({
+      ...formData,
+      purpose: (selectedPurpose || "") as CreateRoomFormData["purpose"],
+    }),
+    [formData, selectedPurpose],
+  );
+
+  const isFormValid = useMemo(() => {
+    return (
+      finalFormData.roomName.trim() !== "" &&
+      finalFormData.capacity >= MIN_PARTICIPANT &&
+      finalFormData.capacity <= MAX_PARTICIPANT &&
+      finalFormData.purpose !== ""
+    );
+  }, [finalFormData]);
+
+  return {
+    formData: finalFormData,
+    updateRoomName,
+    updateCapacity,
+    updatePurpose,
+    isPurposeSelected,
+    isFormValid,
+  };
+};
