27bd5973ddbe6a81ea569da7c77c5ab59a66ca39
kimgho <kgh67811@knu.ac.kr>
2025-09-25 02:00:20 +0900

    feat(package): vitest 추가

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    exclude: ["node_modules", "dist", "e2e", "**/node_modules/**", "**/dist/**", "**/e2e/**"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
