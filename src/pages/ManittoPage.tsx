ea54e78abec0808db4587a6198fe1cb264fc404a
kimgho <kgh67811@knu.ac.kr>
2025-10-05 01:17:32 +0900

    feat(navigation): 각 게임 페이지에서 뒤로가기 동기화 추가

--- src/pages/ManittoPage.tsx
+++ src/pages/ManittoPage.tsx
@@ -1,9 +1,12 @@
 import { ProfileCard } from "@/components/profileview";
 import type { Profile } from "@/components/profileview/types";
+import { useHandleBackPage, useStageNavigation } from "@/hooks";
 import { AppScreen } from "@stackflow/plugin-basic-ui";
 import type { ActivityComponentType } from "@stackflow/react/future";
 
 const ManittoPage: ActivityComponentType<"ManittoPage"> = () => {
+  useStageNavigation();
+  const handleBack = useHandleBackPage();
   const manittoProfile: Profile = {
     profileId: 1,
     name: "김민수",
@@ -15,15 +18,22 @@ const ManittoPage: ActivityComponentType<"ManittoPage"> = () => {
   };
 
   return (
-    <AppScreen appBar={{ title: "마니또" }}>
+    <AppScreen
+      appBar={{
+        title: "마니또",
+        backButton: {
+          onClick: () => {
+            handleBack();
+          },
+        },
+      }}
+    >
       <main className="bg-gradient-primary min-h-screen p-4">
         <div className="flex flex-col items-center">
-          {/* 상단 안내 텍스트 */}
           <div className="mb-6 text-center">
             <p className="text-xl font-semibold text-black">나의 마니또를 확인하세요</p>
           </div>
 
-          {/* 마니또 프로필 카드 */}
           <ProfileCard profile={manittoProfile} />
         </div>
       </main>
