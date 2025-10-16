a37323205e7649e60e0f0c228ada434a8ca837b5
kimgho <kgh67811@knu.ac.kr>
2025-10-17 00:39:14 +0900

    fix(component): role 추가

--- src/components/common/ErrorMessage.tsx
+++ src/components/common/ErrorMessage.tsx
@@ -1,3 +1,8 @@
-export const ErrorMessage = ({ error }: { error?: string | boolean }) => {
-  return <p className="pt-1 pl-2 text-xs text-red-500">{error}</p>;
+export const ErrorMessage = ({ error }: { error?: string }) => {
+  if (!error) return null;
+  return (
+    <p className="pt-1 pl-2 text-xs text-red-500" role="alert">
+      {error}
+    </p>
+  );
 };
