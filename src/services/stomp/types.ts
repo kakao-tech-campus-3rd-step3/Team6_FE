2f8b20d473e43d2046ae3d105724ca23ab524d12
kimgho <kgh67811@knu.ac.kr>
2025-10-27 01:59:52 +0900

    feat(hooks): react-router-dom으로 변경

--- src/services/stomp/types.ts
+++ src/services/stomp/types.ts
@@ -1,14 +1,18 @@
 import type { StompError } from "@/errors/stomp-errors";
+import type { NavigateFunction } from "react-router-dom";
 
 export interface StompState {
   isConnected: boolean;
   isConnecting: boolean;
   error: StompError | null;
 }
+interface RoomChangeData {
+  stage: string;
+}
+export type RoomChangeResponse = BaseResponse<RoomChangeData>;
 
 export type StateListener = (state: StompState) => void;
 
 export type Unsubscribe = () => void;
 
-export type PushFunction = (activity: string, params: Record<string, string>) => void;
-export type ReplaceFunction = (activity: string, params: Record<string, string>) => void;
+export type NavigateFn = NavigateFunction;
