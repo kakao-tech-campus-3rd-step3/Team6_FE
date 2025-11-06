19f765a6144327b493bd81dceb01d6816b72a28e
kimgho <kgh67811@knu.ac.kr>
2025-11-07 01:19:10 +0900

    feat(page): 룰렛 스켈레톤 UI추가

--- src/components/randomroulette/index.tsx
+++ src/components/randomroulette/index.tsx
@@ -1,4 +1,5 @@
 export { RandomRouletteQuestion } from "./RandomRouletteQuestion";
 export { RandomRouletteTip } from "./RandomRouletteTip";
 export { Roulette } from "./Roulette";
+export { RouletteSkeleton } from "./RouletteSkeleton";
 export { WinnerModal } from "./WinnerModal";
