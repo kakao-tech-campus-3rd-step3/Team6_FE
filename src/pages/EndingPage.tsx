8a71d8098109ccde3f0ee15fffc1af6ba64ccdfe
kimgho <kgh67811@knu.ac.kr>
2025-11-07 01:36:22 +0900

    fix(page): 홈버튼 추가 및 페이지 마운트 시 한 번만 초기화하도록 수정

--- src/pages/EndingPage.tsx
+++ src/pages/EndingPage.tsx
@@ -1,34 +1,36 @@
 import { Button } from "@/components/common";
 import { EndingComment, SaveComment } from "@/components/ending";
-import { useHandleBackPage, useStageNavigation } from "@/hooks";
 import { PageLayout } from "@/layouts/PageLayout";
 import { useAuthStore } from "@/store/authStore";
 import { useUserStore } from "@/store/useUserStore";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const EndingPage = () => {
-  useStageNavigation();
-  const { handleBack, canGoBack } = useHandleBackPage();
   const clearAuth = useAuthStore((state) => state.clearAuth);
   const clearUser = useUserStore((state) => state.clearParticipants);
+  const navigate = useNavigate();
 
-  clearAuth();
-  clearUser();
+  useEffect(() => {
+    clearAuth();
+    clearUser();
+  }, [clearAuth, clearUser]);
 
   return (
     <PageLayout
       appBar={{
         title: "마무리",
-        backButton: canGoBack
-          ? {
-              onClick: handleBack,
-            }
-          : { render: () => null },
       }}
     >
       <main className="bg-gradient-primary space-y-4 p-4 pb-8">
         <EndingComment />
         <SaveComment />
-        <Button className="h-12 text-lg font-semibold">프로필 저장하기</Button>
+        <div className="flex flex-row justify-around gap-2">
+          <Button className="h-12 text-lg font-semibold">프로필 저장하기</Button>
+          <Button className="h-12 text-lg font-semibold" variant="sub" onClick={() => navigate("/")}>
+            홈으로
+          </Button>
+        </div>
       </main>
     </PageLayout>
   );
