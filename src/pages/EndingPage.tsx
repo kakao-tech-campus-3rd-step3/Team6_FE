9a168d751e6eed14634f50a03fd6ca8d171fbeaf
kimgho <kgh67811@knu.ac.kr>
2025-11-07 01:21:03 +0900

    feat(page): 엔딩 페이지에서 localStorage 초기화하도록 추가

--- src/pages/EndingPage.tsx
+++ src/pages/EndingPage.tsx
@@ -1,10 +1,30 @@
 import { Button } from "@/components/common";
 import { EndingComment, SaveComment } from "@/components/ending";
+import { useHandleBackPage, useStageNavigation } from "@/hooks";
 import { PageLayout } from "@/layouts/PageLayout";
+import { useAuthStore } from "@/store/authStore";
+import { useUserStore } from "@/store/useUserStore";
 
 const EndingPage = () => {
+  useStageNavigation();
+  const { handleBack, canGoBack } = useHandleBackPage();
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const clearUser = useUserStore((state) => state.clearParticipants);
+
+  clearAuth();
+  clearUser();
+
   return (
-    <PageLayout appBar={{ title: "마무리" }}>
+    <PageLayout
+      appBar={{
+        title: "마무리",
+        backButton: canGoBack
+          ? {
+              onClick: handleBack,
+            }
+          : { render: () => null },
+      }}
+    >
       <main className="bg-gradient-primary space-y-4 p-4 pb-8">
         <EndingComment />
         <SaveComment />
